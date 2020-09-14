fd77a0db53a7353b336859f11912d373c6b37fe9
Yarolmj <yarolmj@gmail.com>
2020-09-14 12:32:07 -0600

    commit on 'master'

--- load_text/text_load.asm
+++ load_text/text_load.asm
@@ -1,17 +1,21 @@
 section .data
-    instruction_buffer: TIMES 330000 dq '*' ;10000 instrucciones * 32 bit + 10000 saltos de linea = limite del buffer de instrucciones es 10000
-    filename: db "/home/yarol/MIPS x86/MIPS_2_x86_emu/MIPS TEST/mips1.txt",0
-
+    instruction_buffer: TIMES 330000 db '*' ;10000 instrucciones * 32 bit + 10000 saltos de linea = limite del buffer de instrucciones es 10000
+    filename_text: db "/home/yarol/MIPS x86/MIPS_2_x86_emu/MIPS TEST/mips1.txt",0
 section .text
 global _start
 
 
+_wait_for_input:
+; parametros
+; 0: text
+; 1: data
+; 2
 
 
 _read_text:
     ;file open
     mov rax,2
-    mov rdi,filename
+    mov rdi,filename_text
     mov rsi,0
     mov rdx,0
     syscall
@@ -19,7 +23,7 @@ _read_text:
     mov rdi,rax
     mov rax,0
     mov rsi,instruction_buffer
-    mov rdx,33*2
+    mov rdx,(32+1)*10000
     syscall
     ;print the readed information
     mov rax,1
@@ -34,6 +38,7 @@ _exit:
     mov rdi,0
     syscall
 _start:
+    call _wait_for_input
     call _read_text
     call _exit
 
