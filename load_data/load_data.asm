c9de61239d1a43b90603ff76a3c709b5a6650404
Andrub21 <andrub.rb@gmail.com>
2020-09-14 13:39:14 -0600

    commit on master

--- load_data/load_data.asm
+++ load_data/load_data.asm
@@ -1,31 +1,28 @@
 section .data
-    instruction_buffer: TIMES 330000 dq '*' ;10000 instrucciones * 32 bit + 10000 saltos de linea = limite del buffer de instrucciones es 10000
-    filename: db "/home/yarol/MIPS x86/MIPS_2_x86_emu/MIPS TEST/mips1.txt",0
+    data_buffer: TIMES 33792 db '*' ;10000 instrucciones * 32 bit + 10000 saltos de linea = limite del buffer de instrucciones es 10000
+    filename_data: db "/home/ruben/Downloads/II Semestre 2020/Arqui/I proyecto/MIPS_2_x86_emu/MIPS TEST/pong .data",0
 
 section .text
 global _start
 
-
-
-
 _read_text:
     ;file open
     mov rax,2
-    mov rdi,filename
+    mov rdi,filename_data
     mov rsi,0
     mov rdx,0
     syscall
     ;file read to buffer
     mov rdi,rax
     mov rax,0
-    mov rsi,instruction_buffer
-    mov rdx,33*2
+    mov rsi,data_buffer
+    mov rdx,33*1024
     syscall
     ;print the readed information
     mov rax,1
     mov rdi,0
-    mov rsi,instruction_buffer
-    mov rdx,33*2
+    mov rsi,data_buffer
+    mov rdx,33*1024
     syscall
     ret
 _exit:
